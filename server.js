719901bd0b5b012e2633d8acc974ee0db446e934
jayjay Anyim <53899996+jayjayh577@users.noreply.github.com>
2024-01-27 12:14:12 +0000

    new commit

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.post('/collections/:collectionName', async function (req, res, next) {
 
 
 // Start the server
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
